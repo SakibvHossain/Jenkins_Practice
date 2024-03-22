1a1b523c369c1047e4539ebea1e8630b63f92640
SakibvHossain <sakibxhossian021@gmail.com>
2024-03-22 14:43:04 +0600

    first commit

---
+++ src/test/java/com/example/jenkins_demo_project/JenkinsDemoProjectApplicationTests.java
@@ -0,0 +1,22 @@
+package com.example.jenkins_demo_project;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemoProjectApplicationTests {
+
+    public static Logger log = LoggerFactory.getLogger(JenkinsDemoProjectApplicationTests.class);
+
+    @Test
+    void contextLoads() {
+        log.info("Application test executed...");
+        int x = 2;
+        int y = 3;
+        Assertions.assertEquals(6, x*y);
+    }
+
+}
